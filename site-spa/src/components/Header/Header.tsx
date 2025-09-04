0f09ddeac442107bea8cb38dc2df2559673c4f9d
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-09-04 20:49:18 -0300

    refactor: adiciona componente NavBar no Header

--- site-spa/src/components/Header/Header.tsx
+++ site-spa/src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
+import NavBar from "../NavBar/NavBar";
+
 export default function Header(){
     return(
         <header>
             <h1>Acompanha+</h1>
+            <NavBar/>
         </header>
     );
 }
