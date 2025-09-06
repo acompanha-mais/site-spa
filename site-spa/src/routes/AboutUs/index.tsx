265e5803213fbe1517a85602300d81c3bdc6c263
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-09-06 15:24:22 -0300

    feat: cria rota AboutUs

---
+++ site-spa/src/routes/AboutUs/index.tsx
@@ -0,0 +1,13 @@
+export default function AboutUs(){
+    return(
+        <main>
+            <h2>Quem somos?</h2>
+            
+            <p>O Acompanha+ nasceu para atender a uma necessidade real dos pacientes do Hospital das Clínicas: reduzir as dificuldades no acesso às teleconsultas e melhorar a adesão aos atendimentos de saúde.</p>
+
+            <p>Observamos que muitos pacientes enfrentam barreiras tecnológicas, como dificuldade em utilizar aplicativos ou acessar links de consulta, o que resultava em faltas e atrasos nos atendimentos. Pensando nisso, criamos uma solução simples e humana: conectar voluntários preparados para oferecer suporte prático e garantir que cada paciente consiga participar de sua consulta online.</p>
+
+            <p>Nosso propósito é aproximar pessoas da saúde que precisam, unindo tecnologia e solidariedade para transformar a experiência dos pacientes e apoiar o trabalho dos profissionais de saúde.</p>
+        </main>
+    );
+}   
