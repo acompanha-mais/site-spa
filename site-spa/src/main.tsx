a6be752df9c094a1fe8d980510e0b95efbeed39c
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-09-06 19:39:06 -0300

    refactor: rotas AboutUs e SignUpCuidador referenciadas

--- site-spa/src/main.tsx
+++ site-spa/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import Home from './routes/Home/index.tsx'
 import AboutUs from './routes/AboutUs/index.tsx'
+import SignUpCuidador from './routes/SignUpCuidador/index.tsx'
 
 import Error from './routes/Error/index.tsx'
 
@@ -12,10 +13,12 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 
 
+
 const router = createBrowserRouter([
   {path: "/", element: <App/>, errorElement: <Error/>, children: [
     {path: "/", element: <Home/>},
-    {path: "/sobre", element: <AboutUs/>}
+    {path: "/sobre", element: <AboutUs/>},
+    {path: "/cadastro/cuidador", element: <SignUpCuidador/>}
   ]}
 ]);
 
