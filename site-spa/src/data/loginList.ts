b911ead1bbbbc7e07f0f8a5cdfca3e7bf3e2e710
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-09-06 19:40:23 -0300

    feat: cria lista de login para testes

---
+++ site-spa/src/data/loginList.ts
@@ -0,0 +1,7 @@
+import type { TipoLogin } from "../types/loginTypes"
+
+export const listaLogins:TipoLogin[] = [
+    {cpf: "123412", senha: "431313", tipo: "paciente"},
+    {cpf: "912919", senha: "381712", tipo: "cuidador"},
+    {cpf: "021011", senha: "331399", tipo: "paciente"}
+];
