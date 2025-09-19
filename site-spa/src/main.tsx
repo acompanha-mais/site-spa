c383a4349ecb0846ceff3933e05cec04492ddc0c
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-09-19 15:04:55 -0300

    feat: tailwindcss instalado e fonte global aplicada

--- site-spa/src/main.tsx
+++ site-spa/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './globals.css'
+
 
 import App from './App.tsx'
 import Home from './routes/Home/index.tsx'
