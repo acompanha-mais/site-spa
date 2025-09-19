5d35ad47ad16551768ae59d8fe4344746f201580
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-09-19 16:49:54 -0300

    style: estiliza rota Home aumentando a fonte e espessura do título

--- site-spa/src/routes/Home/index.tsx
+++ site-spa/src/routes/Home/index.tsx
@@ -1,7 +1,7 @@
 export default function Home(){
     return(
         <main>
-            <h2>Bem vindo a Acompanha+</h2>
+            <h2 className="text-3xl font-bold">Bem vindo a Acompanha+</h2>
         </main>
     );
 }
