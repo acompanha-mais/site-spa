00d3b544e7b23b5b9e511c24d5fa3d838df1fe9c
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-09-20 13:46:08 -0300

    style(header): altera tamanho da logo

--- site-spa/src/components/Header/Header.tsx
+++ site-spa/src/components/Header/Header.tsx
@@ -2,8 +2,8 @@ import NavBar from "../NavBar/NavBar";
 
 export default function Header(){
     return(
-        <header className="bg-white shadow-lg p-6">
-            <h1  className="text-2xl font-bold">Acompanha+</h1>
+        <header className="bg-white shadow-lg p-4">
+            <p className="font-semibold text-2xl">Acompanha+</p>
             <NavBar/>
         </header>
     );
