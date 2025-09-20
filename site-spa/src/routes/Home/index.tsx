4fc48351ca1d7d253e0ef773f45f8dccc5f96c7e
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-09-20 13:45:39 -0300

    style(home): estiliza rota Home, adiciona imagem e estilizações globais aplicadas no globals.css

--- site-spa/src/routes/Home/index.tsx
+++ site-spa/src/routes/Home/index.tsx
@@ -1,7 +1,25 @@
-export default function Home(){
-    return(
+export default function Home() {
+    return (
         <main>
-            <h2 className="text-3xl font-bold">Bem vindo a Acompanha+</h2>
+            <section className="flex flex-col md:flex-row items-center justify-between md:gap-24">
+                <div className="w-full md:w-1/2 text-center md:text-left">
+                    <h1 className="text-3x1  font-bold pb-4">
+                        Ajude quem precisa! Faça a diferença!
+                    </h1>
+                    <p>
+                        Sua contribuição pode transformar vidas. Saiba como você pode ajudar e fazer parte dessa mudança agora mesmo.
+                    </p>
+                    <button className="mt-4">Saiba mais</button>
+                </div>
+
+                <img
+                    src="/home-img.png"
+                    alt="Cuidadora ajudando idosa"
+                    className="w-3/4 md:w-1/3 md:min-w-[250px] mt-8 md:mt-0 rounded-lg shadow-lg"
+                />
+            </section>
+
+            
         </main>
     );
 }
