2992be6aa540716bc75cf9e6735989a0a96d54f8
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-09-20 19:28:35 -0300

    feat: adiciona menu hamburguer

--- site-spa/src/components/Header/Header.tsx
+++ site-spa/src/components/Header/Header.tsx
@@ -2,9 +2,11 @@ import NavBar from "../NavBar/NavBar";
 
 export default function Header(){
     return(
-        <header className="bg-white shadow-lg p-4">
-            <p className="font-semibold text-2xl">Acompanha+</p>
-            <NavBar/>
+        <header className="bg-white shadow-lg p-4 flex items-center justify-between px-[15%]">
+            <p className="font-semibold text-2xl -ml-2xs">Acompanha+</p>
+            <div>
+                <NavBar/>
+            </div>
         </header>
     );
 }
--- site-spa/src/components/NavBar/NavBar.tsx
+++ site-spa/src/components/NavBar/NavBar.tsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function NavBar(){
+    const[isOpen, setIsOpen] = useState(false);
+
     return(
-        <nav>
-            <Link to="/">Início</Link> |
-            <Link to="/sobre"> Sobre nós</Link> |
-            <Link to="/cadastro"> Cadastro</Link> |
-            <Link to="/time">Time</Link>
+        <nav className="relative">
+            <div className="hidden md:flex space-x-12">
+                <Link to="/">Início</Link>
+                <Link to="/sobre"> Sobre nós</Link>
+                <Link to="/cadastro"> Cadastro</Link>
+                <Link to="/time">Time</Link>
+            </div>
+            
+            <div className="md:hidden">
+                <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="[all:unset] text-6xl cursor-pointer"
+                >
+                    {isOpen ? "X" : "☰"}
+                </button>
+
+                <div
+                    className={`absolute right-0 mt-2 w-40 bg-white shadow-lg rounded-lg overflow-hidden transition-all duration-300 ease-in-out
+                    ${isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"}`}
+                >
+                    <div className="flex flex-col p-4 space-y-2">
+                        <Link to="/" onClick={() => setIsOpen(false)}>Início</Link>
+                        <Link to="/sobre" onClick={() => setIsOpen(false)}>Sobre nós</Link>
+                        <Link to="/cadastro" onClick={() => setIsOpen(false)}>Cadastro</Link>
+                        <Link to="/time" onClick={() => setIsOpen(false)}>Time</Link>
+                    </div>
+                </div>
+            </div>
         </nav>
     );
 }
