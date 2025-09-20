6cfb80e50d97dcf87771c7161fe70ad5da0af00d
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-09-20 18:46:23 -0300

    style(footer): adiciona sombra interna e centraliza

--- site-spa/src/components/Footer/Footer.tsx
+++ site-spa/src/components/Footer/Footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer(){
     return(
-        <footer>
+        <footer className="text-center bg-white p-6 shadow-inner shadow-gray-300">
             <p>&copy; Todos os direitos reservados - 2025 | Acompanha+</p>
+            <p>Desenvolvido com ❤️ pelo time Acompanha+</p>
         </footer>
     );
 }
