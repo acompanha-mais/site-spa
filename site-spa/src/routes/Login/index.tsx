ffc4ce202caaf37f04ac1dfb589a6105f953978a
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-09-27 19:33:41 -0300

    refactor(login): troca useState por Hook Form

--- site-spa/src/routes/Login/index.tsx
+++ site-spa/src/routes/Login/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
+import type { TipoLogin } from "../../types/typeLogin";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
     useEffect(() => {
@@ -8,17 +9,14 @@ export default function Login() {
     }, []);
 
     const navigate = useNavigate();
-    const[email, setEmail] = useState("");
-    const[senha, setSenha] = useState("");
     
-    
-    function verificarLogin(e:React.FormEvent) {
-        e.preventDefault();
-        
+    const { register, handleSubmit, formState: { errors } } = useForm<TipoLogin>();
+
+    const onSubmit = (data: TipoLogin) => {
         const listaUsuarios = JSON.parse(sessionStorage.getItem("usuarios") || "[]");
 
         const usuario = listaUsuarios.find(
-            (u:any) => u.email === email && u.senha === senha
+            (u:any) => u.email === data.email && u.senha === data.senha
         );
 
         if (!usuario){
@@ -30,32 +28,29 @@ export default function Login() {
         sessionStorage.setItem("logado", "true");
 
         alert("Login realizado com sucesso!")
-
         navigate("/perfil-cuidador");
         window.location.reload();
-    }
+    };
 
     return(
         <main className="min-h-screen flex flex-col items-center">
             <h1 className="mb-20">Login</h1>
-            <form onSubmit={verificarLogin} className="bg-white shadow-md rounded-lg p-6 w-full max-w-md flex flex-col space-y-4">
+            <form onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-md rounded-lg p-6 w-full max-w-md flex flex-col space-y-4">
                 <input 
                     type="email"
                     placeholder="Email"
-                    value={email}
-                    onChange={e=> setEmail(e.target.value)}
-                    required
+                    {...register("email", { required: "O email é obrigatório" })}
                     className="border rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-600"
                 />
+                {errors.email && <span className="text-red-600">{errors.email.message}</span>}
                 <input 
                     type="password"
                     placeholder="Senha" 
-                    value={senha}
-                    onChange={e=> setSenha(e.target.value)}
-                    required
+                    {...register("senha", {required: "A senha é obrigatória", minLength: { value: 8, message: "A senha deve ter no mínimo 8 caracteres" }})}
                     className="border rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-red-600"
                 />
-
+                {errors.senha && <span className="text-red-600">{errors.senha.message}</span>}
+                
                 <button type="submit" className="mx-auto w-[130px] md:w-[176px] mt-4">Entrar</button>
             </form>
         </main>
