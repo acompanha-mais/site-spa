a27f31ab83b5d94eb62976e2b5d35b4f4678c145
Matheus-Vecchi <matheusvecchi02@gmail.com>
2025-11-09 17:42:41 -0300

    fix: corrige F5

--- site-spa/vite.config.ts
+++ site-spa/vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-  react(),
-  tailwindcss()
-  ]
+  plugins: [react(), tailwindcss()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
